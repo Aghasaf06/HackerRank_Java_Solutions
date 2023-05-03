dba07310b8d7d5c9aee94d38f8d52aa4640ef494
Aghasaf06 <agasfm90@gmail.com>
2023-05-04 02:42:30 +0400

    Java (Basic) | Easy

---
+++ DataStructures/Java Generics/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static < T > void printArray(T[] inputArray){
+        for(T element : inputArray){
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
